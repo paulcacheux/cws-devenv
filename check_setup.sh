43b2c9855b19820b39a07feba2a939868e89e20d
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-05-10 15:05:23 +0200

    test: use separate script to test common invoke tasks

---
+++ check_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+cd ~/dd/datadog-agent
+
+# general build
+inv -e system-probe.build
+inv -e security-agent.build
+
+# functional tests
+inv -e security-agent.build-embed-syscall-tester
+inv -e security-agent.build-embed-latency-tools
+inv -e security-agent.build-functional-tests
